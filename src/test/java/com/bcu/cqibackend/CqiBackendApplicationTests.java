e3bd4e198d4f8d99fc3c2df7d0838e6befc6e3d0
Ramindu Samarawickrama <ramindu.samarawickrama@gmail.com>
2021-11-20 18:28:48 +0000

    Init Commit

---
+++ src/test/java/com/bcu/cqibackend/CqiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcu.cqibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
